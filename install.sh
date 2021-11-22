514214e3354090d576e60a076ee057cfa1fcb37d
João Paulo F. Nunes <fnunes.joao@gmail.com>
2021-11-22 15:54:54 -0800

    Installs additional dependencies to install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 sudo apt update
-sudo apt install -y python3 git
+sudo apt install -y python3 python3-distutils git make
 
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python3 /tmp/get-pip.py --user
 python3 -m pip install --user ansible
 
-git clone git@github.com:joao-fnunes/dotfiles.git
+git clone https://github.com/joao-fnunes/dotfiles.git
 
 cd dotfiles
 
