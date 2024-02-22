b3a0673b320bb20552db5596ebf2b29fba9fae16
João Paulo F. Nunes <fnunes.joao@gmail.com>
2024-02-22 14:07:01 -0800

    Update install.sh to clone dotfiles through HTTPS

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python3 /tmp/get-pip.py --user
 python3 -m pip install --user ansible
 
-git clone git@github.com:joao-fnunes/dotfiles
+git clone https://github.com/joao-fnunes/dotfiles.git
 
 cd dotfiles
 
