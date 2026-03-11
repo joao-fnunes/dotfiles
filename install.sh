715360ae6d17a90da9d7e3e4b0510270fdb30026
Joao Nunes <fnunes.joao@gmail.com>
2026-03-11 18:43:37 +0000

    Improves tmux config, new installation

--- install.sh
+++ install.sh
@@ -3,19 +3,17 @@
 source /etc/os-release
 
 if [[ $ID == "arch" ]]; then
-	sudo pacman -Sy python3 git make
+  sudo pacman -Sy python3 python3-pip ansible git make
+elif [[ $ID == "ubuntu" ]]
+  sudo apt update
+  sudo apt install -y python3 python3-pip ansible git make
 else
-	sudo apt update
-	sudo apt install -y python3 python3-distutils git make
+  echo "Unknown distro"
+  exit 1
 fi
-
-curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python3 /tmp/get-pip.py --user
-python3 -m pip install --user ansible
 
 git clone https://github.com/joao-fnunes/dotfiles.git
 
 cd dotfiles
 
-export PATH="/home/$USER/.local/bin:$PATH"
 make bootstrap && make install
